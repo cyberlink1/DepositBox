04ef193a963795a4d58c03780aec30570c5be258
Cyber Link <cl@xganon.com>
2022-06-06 20:15:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,17 +9,13 @@ sudo lighty-enable-mod fastcgi
 sudo lighty-enable-mod fastcgi-php
 echo "extension=gnupg.so" >> /etc/php/8.1/cgi/conf.d/20-gnupg.ini
 sudo service lighttpd force-reload
+sudo cp DepositBox /var/www/html
 mkdir /var/www/.gpg
 cd /var/www/html
-
-#Still need to work on this
-#sudo git clone https://github.com/cyberlink1/DepositBox.git
-#sudo chown -R www-data:www-data DepositBox
+sudo chown -R www-data:www-data DepositBox
 
 #set lighttpd to service.bind = 127.0.0.1
 
-
-#get https://raw.githubusercontent.com/pastly/python-snippits/master/src/tor/x25519-gen.py save as tor-client-keygen.py
 #edit torrc to turn on hidden service for 80 and 22
 #start tor will create /var/lib/hidden_service/authorized_clients
 #create file in above dir <name>.auth (contains descriptor:x25519:<public-key>
